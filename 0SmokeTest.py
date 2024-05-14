070fa5d9e5725a9a57502b2bac98459cd10c6ce0
Tylan Jensen <tylan.cjensen@gmail.com>
2024-05-14 00:37:42 -0700

    Set up for accepting outside traffic

--- 0SmokeTest.py
+++ 0SmokeTest.py
@@ -1,11 +1,30 @@
 #Common python convetion for running a main function.
 
-from socket import IP_DEFAULT_MULTICAST_LOOP
+import socket
 
 
 def main():
+    HOST = "0.0.0.0"
+    PORT = 65432
 
-    print("hello world")
+    #creates a socket that supports context manager type. 
+    #AF_INET internet adress for IPv4 expects two-tuple (host,port)
+    #SOCK_STREAM socket type for TCP
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+        #.bind() method assosciates specific network interface and port number
+        s.bind((HOST,PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                print(data)
+                if not data:
+                    break
+                conn.sendall(data)
+
+    
 #guard the main function call with an if statement. 
 # __name is the name of the file in the context of how the name is envoked.
 #reason, you don't want to run the main function when importing.
