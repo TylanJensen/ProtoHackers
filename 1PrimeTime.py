9e5e9fea744a13b923af8d1f3d9b58582fb7e450
Tylan Jensen <tylan.cjensen@gmail.com>
2024-05-15 01:59:20 -0700

    added code for threading

--- 1PrimeTime.py
+++ 1PrimeTime.py
@@ -1,6 +1,40 @@
 import socket
 import json
 from math import sqrt
+from threading import Thread
+
+def threadedFunction(conn,addr):
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            while True:
+                data = conn.recv(1024)
+                print(data)
+                dataDic = json.loads(data)
+                if dataDic["method"] == "isPrime" and type(dataDic["number"]) == int :
+                    print("You did it")
+                    n = dataDic["number"]
+                    primeBool = 1
+                    if (n > 1):
+                        for i in range (2, int(sqrt(n)) + 1):
+                            if(n % i == 0):
+                                primeBool = 0
+                                break
+                        if(primeBool == 1):
+                            print("Prime")
+                            returnData = {"method":"isPrime","prime":bool(primeBool)}
+                            
+                            
+                            conn.sendall(bytes(json.dumps(returnData), 'utf-8'))
+                        else:
+                            print("Not Prime")
+                            returnData = {"method":"isPrime","prime":bool(primeBool)}
+                            conn.sendall(bytes(json.dumps(returnData), 'utf-8'))
+                                        
+                else:
+                    print("Failure")
+                    conn.sendall(data)
+                    break
 
 def main():
     HOST = "0.0.0.0"
@@ -16,42 +50,9 @@ def main():
         s.listen()
         while True:
             conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    print(data)
-                    dataDic = json.loads(data)
-                    if dataDic["method"] == "isPrime" and type(dataDic["number"]) == int :
-                        print("You did it")
-                        n = dataDic["number"]
-                        primeBool = 1
-                        if (n > 1):
-                            for i in range (2, int(sqrt(n)) + 1):
-                                if(n % i == 0):
-                                    primeBool = 0
-                                    break
-                            if(primeBool == 1):
-                                print("Prime")
-                                returnData = {"method":"isPrime","prime":bool(primeBool)}
-                                
-                                
-                                conn.sendall(bytes(json.dumps(returnData), 'utf-8'))
-                            else:
-                                print("Not Prime")
-                                returnData = {"method":"isPrime","prime":bool(primeBool)}
-                                conn.sendall(bytes(json.dumps(returnData), 'utf-8'))
-                                          
-                    else:
-                        print("Failure")
-                        conn.sendall(data)
-                        break
-                        
-                    
-                        
-
-        
-                    
+            thread = Thread(target = threadedFunction, args = (conn,addr))
+            thread.start()
+            thread.join()                    
 
 if __name__ == "__main__":
     main()
